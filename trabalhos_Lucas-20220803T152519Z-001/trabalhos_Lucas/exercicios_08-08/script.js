2ad70d1001868a3a10d91c45030e60179e3e3748
Lucas Marcelo Schier <ens-lucasschier@uniguacu.edu.br>
2022-08-08 22:13:51 -0300

    teste 01

---
+++ trabalhos_Lucas-20220803T152519Z-001/trabalhos_Lucas/exercicios_08-08/script.js
@@ -0,0 +1,13 @@
+function verificar() {
+    let nome = document.getElementById('nome'.value)
+    let ano = data.getFullYear()
+    let nascimento = document.getElementById('nascimento'.value)
+    let res = document.getElementById('res')
+    if (nascimento.value == 0 || Number(nascimento.value) > ano) {
+        window.alert('[ERRO] Ano Inválido - Verifique os dados e tente novamente [ERRO]')
+    } else () {
+        alert(ano)
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `${nome} com ${idade} anos.`
+}
